d7b4497ff9d795989ef72260ef605433e7da655d
Michael McCune <msm@redhat.com>
2016-02-15 17:48:34 -0500

    convert app to full python style
    removing the .sti custom assemble and run commands in favor of the
    upstream advice for python apps in the centos builder[1].
    
    [1]: https://hub.docker.com/r/centos/python-27-centos7/

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+import shiny_squirrel
+
+
+mongodb_service_name = os.getenv('MONGODB_SERVICE_NAME')
+if mongodb_service_name is None:
+    print('MONGODB_SERVICE_NAME not provided')
+    sys.exit(-1)
+
+mongodb_username = open('/etc/mongo-secret/username').read()
+mongodb_password = open('/etc/mongo-secret/password').read()
+
+mongo_url = 'mongodb://{user}:{password}@{service}/sparkhara'.format(
+            user=mongodb_username,
+            password=mongodb_password,
+            service=mongodb_service_name)
+
+shiny_squirrel.main(mongo_url)
