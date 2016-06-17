f1cf285350e7c814e87acb6e3238b12d1438f529
Michael McCune <msm@redhat.com>
2016-06-17 16:10:57 -0400

    remove secret stuff from app.py
    This change is to simplify some things for testing.

--- app.py
+++ app.py
@@ -4,17 +4,22 @@ import sys
 import shiny_squirrel
 
 
-mongodb_service_name = os.getenv('MONGODB_SERVICE_NAME')
-if mongodb_service_name is None:
-    print('MONGODB_SERVICE_NAME not provided')
+mongo_url = os.getenv('SHINY_SQUIRREL_MONGO_URL')
+if mongo_url is None:
+    print('SHINY_SQUIRREL_MONGO_URL not provided')
     sys.exit(-1)
 
-mongodb_username = open('/etc/mongo-secret/username').read()
-mongodb_password = open('/etc/mongo-secret/password').read()
+# mongodb_service_name = os.getenv('MONGODB_SERVICE_NAME')
+# if mongodb_service_name is None:
+#     print('MONGODB_SERVICE_NAME not provided')
+#     sys.exit(-1)
 
-mongo_url = 'mongodb://{user}:{password}@{service}/sparkhara'.format(
-            user=mongodb_username,
-            password=mongodb_password,
-            service=mongodb_service_name)
+# mongodb_username = open('/etc/mongo-secret/username').read()
+# mongodb_password = open('/etc/mongo-secret/password').read()
+
+# mongo_url = 'mongodb://{user}:{password}@{service}/sparkhara'.format(
+#             user=mongodb_username,
+#             password=mongodb_password,
+#             service=mongodb_service_name)
 
 shiny_squirrel.main(mongo_url)
